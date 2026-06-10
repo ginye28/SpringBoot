12f52ec4747abbf101099da6f4851d2b0f1dc822
ginye1268 <tsna1268@gmail.com>
2026-06-10 17:43:19 +0900

    team project

---
+++ ch06/todo-api/src/main/java/com/korit/todoapi/security/NaverUserInfo.java
@@ -0,0 +1,31 @@
+package com.korit.todoapi.security;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo {
+    private final Map<String, Object> attributes;
+
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = (Map<String, Object>) attributes.get("response");
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getName() {
+        return "name";
+    }
+}
